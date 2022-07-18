3a7b0c5b7ba4e3fa92eb20d2b26061eb43d3e488
TzeLun <loktzelun@gmail.com>
2022-07-19 01:47:42 +0900

    Added PSNR & minor edits

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -12,6 +12,12 @@ list_to_save = ["C:/Users/Tze Lun/DIV2K/x_train/X4_40x40/",
                 "C:/Users/Tze Lun/DIV2K/y_train/HR_160x160/",
                 "C:/Users/Tze Lun/DIV2K/y_valid/HR_160X160/"]
 
+# Folders containing 5 test image from Set5, available on author's Github:
+# https://github.com/alterzero/DBPN-Pytorch/tree/master/Input/Set5_LR_x4
+dir_test_file = "C:/Users/Tze Lun/DIV2K/x_test/LR_original/"
+dir_save = "C:/Users/Tze Lun/DIV2K/x_test/X4_40X40/"
+
+# re-crop the images to the desired size
 for i in range(len(list_to_convert)):
     if i <= 1:
         size = (40, 40)  # width, height
@@ -20,4 +26,5 @@ for i in range(len(list_to_convert)):
         size = (160, 160)
         crop(list_to_convert[i], list_to_save[i], size)
 
-
+size = (40, 40)
+crop(dir_test_file, dir_save, size)
--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from DDBPN import *
 from Data_Eval import *
 
 
-#gpu_available = tf.test.is_gpu_available()
-#is_cuda_gpu_available = tf.test.is_gpu_available(cuda_only=True)
-#
-# print(gpu_available)
-# print(is_cuda_gpu_available)
+# Checks for available gpu
+gpu_available = tf.test.is_gpu_available()
+is_cuda_gpu_available = tf.test.is_gpu_available(cuda_only=True)
+print(gpu_available)
+print(is_cuda_gpu_available)
 
 # Parameters
 choice = "DBPN-M"  # DBPN-M (default), DBPN-M NEF, D-DBPN
@@ -23,9 +23,9 @@ sf = 4  # default at 2, otherwise 4 or 8
 
 # Training the model
 batch_size = 16
-width = 0
-height = 0
-channel = 0
+width = 40
+height = 40
+channel = 3
 input_shape = (width, height, channel)
 lr = 0.0004  # learning rate
 alpha = 0.9  # momentum
@@ -34,9 +34,11 @@ split = 0.2  # ratio of dataset for validation
 
 # Model:
 model_input = Input(shape=input_shape, name=choice)
-# DBPN-M with error feedback (default)
-output = DBPNM(scale_factor=sf)(model_input)
-if choice is "DBPN-M NEF":
+output = 0
+if choice is "DBPN-M":
+    # DBPN-M with error feedback (default)
+    output = DBPNM(scale_factor=sf)(model_input)
+elif choice is "DBPN-M NEF":
     # DBPNM without error feedback
     output = DBPNM_WithoutEF(scale_factor=sf)(model_input)
 elif choice is "D-DBPN":
@@ -49,7 +51,7 @@ model.compile(
     optimizer=tf.keras.optimizers.Adam(learning_rate=lr, beta_1=alpha)
 )
 
-model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=split)
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_valid, y_valid))
 test_result = model.evaluate(x_test, y_test)
 print("Test Loss:", test_result[0])
 
